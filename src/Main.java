21ad9210b5a50f3487e8a41a4d65a8f78b9bf712
jperak <josip.perak@gmail.com>
2025-04-11 20:16:20 +0200

    - exception handling ako su koordinate negativne

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Window window1 = new Window("ProzorPerak");
-        System.out.println("Širina prozora je: " + window1.width(100, 20));
-        System.out.println("Visina prozora je: " + window1.height(40, 15));
-        System.out.println("Površina prozora je: " + window1.area(20, 15, 100, 40));
-        System.out.println("Opseg prozora je: " + window1.perimeter(20, 15, 100, 40));
-        window1.draw(20, 15, 100, 40);
+        try {
+            Window.checkCoordinates(20, 15, 100, 40);
 
-        Window window2 = new Window("DrugiProzor", true, 40, 100, 15, 20, "nekaOznaka");
-        System.out.println(window2.toString());
+            Window window1 = new Window("ProzorPerak");
+            System.out.println("Širina prozora je: " + window1.width(100, 20));
+            System.out.println("Visina prozora je: " + window1.height(40, 15));
+            System.out.println("Površina prozora je: " + window1.area(20, 15, 100, 40));
+            System.out.println("Opseg prozora je: " + window1.perimeter(20, 15, 100, 40));
+            window1.draw(20, 15, 100, 40);
+
+            Window window2 = new Window("DrugiProzor", true, 40, 100, 15, 20, "nekaOznaka");
+            System.out.println(window2.toString());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Pogreška: " + e.getMessage());
+        }
 
     }
 
--- src/Window.java
+++ src/Window.java
@@ -81,7 +81,7 @@ public class Window {
 
     @Override
     public String toString(){
-        return "Naslov: " + getNaslov() + " oznaka: " + getOznaka() + " status:" + isJelAktivan() + " gornji lijevi ugao: " + getX1() + ", " + getY1() + " donji desni ugao: " + getX2() + ", " + getY2();
+        return "Naslov: " + getNaslov() + " oznaka: " + getOznaka() + " status:" + isJelAktivan() + " gornji lijevi kut: " + getX1() + ", " + getY1() + " donji desni kut: " + getX2() + ", " + getY2();
     }
 
     public static int width(int x2, int x1) {
@@ -119,6 +119,15 @@ public class Window {
             System.out.print("*");
         }
         System.out.println();
+    }
+
+    public static void checkCoordinates(int x1, int y1, int x2, int y2) {
+        if (x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0) {
+            throw new IllegalArgumentException("Koordinate ne mogu biti negativne!");
+        }
+
+        System.out.println("Koordinate su valjane: (" + x1 + ", " + y1 + "), (" + x2 + ", " + y2 + ")");
+
     }
 
 }
